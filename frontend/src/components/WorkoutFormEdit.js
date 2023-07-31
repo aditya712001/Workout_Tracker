943ca92f3d09cd3edc463700b8e23de9263a5c15
Aditya Patil <adityaramdaspatil@gmail.com>
2023-08-01 02:46:27 +0530

    pushing files to github

---
+++ frontend/src/components/WorkoutFormEdit.js
@@ -0,0 +1,87 @@
+import { useState } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from '../hooks/useAuthContext'
+// new
+import { useParams } from 'react-router-dom'
+
+const WorkoutFormEdit = () => {
+  // new
+  const { id } = useParams();
+  const { dispatch } = useWorkoutsContext()
+  const { user } = useAuthContext()
+
+  const [title, setTitle] = useState('')
+  const [load, setLoad] = useState('')
+  const [reps, setReps] = useState('')
+  const [error, setError] = useState(null)
+  const [emptyFields, setEmptyFields] = useState([])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!user) {
+      setError('You must be logged in')
+      return
+    }
+
+    const workout = {title, load, reps}
+
+    const response = await fetch('/api/workouts/'+ id, {
+      method: 'PATCH',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+    const json = await response.json()
+
+    if (!response.ok) {
+      setError(json.error)
+      setEmptyFields(json.emptyFields)
+    }
+    if (response.ok) {
+      setTitle('')
+      setLoad('')
+      setReps('')
+      setError(null)
+      setEmptyFields([])
+      dispatch({type: 'UPDATE_WORKOUT', payload: json})
+    }
+  }
+
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Edit Workout</h3>
+
+      <label>Excersize Title:</label>
+      <input 
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className={emptyFields.includes('title') ? 'error' : ''}
+      />
+
+      <label>Load (in kg):</label>
+      <input 
+        type="number"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+        className={emptyFields.includes('load') ? 'error' : ''}
+      />
+
+      <label>Reps:</label>
+      <input 
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+        className={emptyFields.includes('reps') ? 'error' : ''}
+      />
+
+      <button>Edit Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  )
+}
+
+export default WorkoutFormEdit
