aa07af56f0414208b1a0608cbf5fc38ec103f3c0
Aditya Patil <adityaramdaspatil@gmail.com>
2023-08-16 16:07:57 +0530

    cors installed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
@@ -9,6 +9,7 @@ const userRoutes = require('./routes/user')
 const app = express()
 
 // middleware
+app.use(cors())
 app.use(express.json())
 
 app.use((req, res, next) => {
